e08ced66ce94f73c2b0a60981fee972da29a270a
mike.bskim <mike.bskim@gmail.com>
2021-10-28 00:10:14 +0200

    #3 - done

--- bookings-udemy-27/pkg/handlers/handlers.go
+++ bookings-udemy-27/pkg/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	// "GO/trevor/bookings-udemy-27/pkg/render"
 	// "bookings-udemy/pkg/render"
 	"GO/trevor/bookings-udemy-27/pkg/config"
+	"GO/trevor/bookings-udemy-27/pkg/models"
 	"GO/trevor/bookings-udemy-27/pkg/render"
 	"net/http"
 )
@@ -30,10 +31,17 @@ func NewHandlers(r *Repository) {
 
 // Home is the handler for the home page
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the handler for the about page
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	// send the data to the template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
--- bookings-udemy-27/pkg/render/render.go
+++ bookings-udemy-27/pkg/render/render.go
@@ -2,6 +2,7 @@ package render
 
 import (
 	"GO/trevor/bookings-udemy-27/pkg/config"
+	"GO/trevor/bookings-udemy-27/pkg/models"
 	"bytes"
 	"fmt"
 	"html/template"
@@ -19,8 +20,12 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
 // RenderTemplate renders a template
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
 
 	var tc map[string]*template.Template
 	if app.UseCache {
@@ -42,7 +47,9 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
 	buf := new(bytes.Buffer)
 
-	_ = t.Execute(buf, nil)
+	td = AddDefaultData(td)
+
+	_ = t.Execute(buf, td)
 
 	_, err := buf.WriteTo(w)
 	if err != nil {
